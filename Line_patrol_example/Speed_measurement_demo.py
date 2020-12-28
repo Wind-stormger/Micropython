b13b27f757a778edd0f458d22ba7740fe2fad275
Wind-stormger <932226157@qq.com>
2020-12-28 17:55:14 +0800

    Update Speed_measurement_demo.py

--- Line_patrol_example/Speed_measurement_demo.py
+++ Line_patrol_example/Speed_measurement_demo.py
@@ -4,10 +4,11 @@ import microbit as m
 from machine import I2C,Pin
 Duty_cycle_R = 0 
 Duty_cycle_L = 0
-
-Count_R=0
 IR_R=0
-
+IR_R_2=0
+Count_R=0
+once_time=0
+start_time=0
 i2c=I2C(sda=Pin(21),scl=Pin(22),freq=10000) #I2C initialization
 pca9685 = pca9685.PCA9685(i2c,address=0x40) #Reference to pca9685 object,I2C address of pca9685
 pca9685.freq(freq=50)
@@ -28,26 +29,31 @@ def PWM_motor():
     pca9685.duty(2,value=int(2048+2047/100*Duty_cycle_L),invert=False)
     pca9685.duty(3,value=int(2047-2047/100*Duty_cycle_L),invert=False)
      
-def Speed_measurement():
-    global IR_R,Count_R
+def Speed_measurement_R():#Test the RPM of the right motor
+    global IR_R,IR_R_2,Count_R,start_time,once_time
     m.pin14.write_digital(1)
     IR_R=m.pin5.read_digital()
-    IR_L=m.pin11.read_digital()
-    if IR_R==0:
-        t1=time.ticks_us()
-        IR_R=m.pin5.read_digital()
-        print("hello1")
-        if IR_R==1:
-            Count_R=Count_R+1
-            print("hello2")
-            print(time.ticks_us()-t1)
-    print("Count_R:" + str(Count_R))
-    time.sleep_ms(10)
-    #print("R:" + str(m.pin5.read_digital()), "L:" + str(m.pin11.read_digital()))
+    if IR_R != IR_R_2:
+        IR_R_2 = IR_R
+        Count_R=Count_R+1
+        #print("Count_R:{:2.0f}".format(Count_R))
+        if Count_R==1:            
+            start_time=time.ticks_ms()
+        if Count_R==24:
+            once_time=time.ticks_ms()-start_time
+            RPM_R=60000/once_time
+            print("RPM_R:{:3.0f}r/min".format(RPM_R))
+            print("once_time:{:5.0f}ms".format(once_time))
+            Count_R=0
+    else:
+        Count_R=Count_R
+        #return RPM_R
+
 while True:
-    Duty_cycle_R = 80 
-    Duty_cycle_L = 80
-    #PWM_motor()
-    
-    Speed_measurement()
+    Speed_measurement_R()
+    Duty_cycle_R = 30 #0~100
+    Duty_cycle_L = 0
+    PWM_motor()
+    #a=Speed_measurement_R()
+    #print(a)
     
