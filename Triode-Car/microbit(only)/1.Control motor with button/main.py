534a295c84714f5281f6f3e0ee167da79e9100de
Wind-stormger <76462385+Wind-stormger@users.noreply.github.com>
2021-06-03 11:26:51 +0800

    update for Object Oriented Programming

--- Triode-Car/microbit(only)/1.Control motor with button/main.py
+++ Triode-Car/microbit(only)/1.Control motor with button/main.py
@@ -1,10 +1,20 @@
 import triodecar
 from microbit import *
+
+#创建对象
+triodecar = triodecar.Triodecar()
+# 通过“对象.方法”调用方法
 triodecar.direction_stop()
 while True:
     if button_a.is_pressed() and button_b.is_pressed():
+        display.show(Image.ARROW_N, delay=0, wait=True, loop=False, clear=False)
         triodecar.direction_foward()
+        sleep(100)
     if button_a.is_pressed():
+        display.show(Image.ARROW_W, delay=0, wait=True, loop=False, clear=False)
         triodecar.direction_right()
     if button_b.is_pressed():
+        display.show(Image.ARROW_E, delay=0, wait=True, loop=False, clear=False)
         triodecar.direction_left()
+
+        
