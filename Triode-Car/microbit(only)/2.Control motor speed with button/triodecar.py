108f2af8b93fb17082dfec08e6168be33d96eabe
Wind-stormger <76462385+Wind-stormger@users.noreply.github.com>
2021-06-04 14:49:43 +0800

    add 2.Control motor speed with button

---
+++ Triode-Car/microbit(only)/2.Control motor speed with button/triodecar.py
@@ -0,0 +1,37 @@
+from microbit import *
+#定义类
+class Triodecar:
+
+#初始化方法
+    def __init__(self):
+        pass
+    
+#添加方法
+    def direction_stop(self):
+        pin14.write_digital(1)
+        pin15.write_digital(1)
+    
+    def direction_foward(self):
+        pin14.write_analog(512)
+        pin15.write_analog(512)
+    
+    def direction_left(self):
+        pin14.write_digital(1)
+        pin15.write_analog(512)
+    
+    def direction_right(self):
+        pin14.write_analog(512)
+        pin15.write_digital(1)
+
+    def left_motor_speed(self,value):
+        if value>=0 and value<=10:
+            pin14.write_analog(int(1023-1023/10*value))
+        else:
+            print('Values range from 0 to 10')
+
+    def right_motor_speed(self,value):
+        if value>=0 and value<=10:
+            pin15.write_analog(int(1023-1023/10*value))
+        else:
+            print('Values range from 0 to 10')
+
