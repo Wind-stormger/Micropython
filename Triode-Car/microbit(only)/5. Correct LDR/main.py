3bf40635a8da9088ec73fd288a358874334ad3d6
Wind-stormger <76462385+Wind-stormger@users.noreply.github.com>
2021-06-15 16:18:11 +0800

    Update main.py

--- Triode-Car/microbit(only)/5. Correct LDR/main.py
+++ Triode-Car/microbit(only)/5. Correct LDR/main.py
@@ -10,8 +10,8 @@ D_value = 0
 # 显示东向箭头，调整可调电阻使左光敏电阻的电压模拟值到450~550的范围内后显示正确图标
 while True:
     display.show(Image.ARROW_E, delay=0, wait=True, loop=False, clear=False)
-    print(triodecar.left_LDR())
     sleep(100)
+    print(triodecar.left_LDR())
     if triodecar.left_LDR() >= 450 and triodecar.left_LDR() <= 550 :
         display.show(Image.YES, delay=0, wait=True, loop=False, clear=False)
         sleep(1000)
@@ -21,8 +21,8 @@ while True:
 # 显示西向箭头，调整可调电阻使右光敏电阻的电压模拟值到450~550的范围内后显示正确图标
 while True:
     display.show(Image.ARROW_W, delay=0, wait=True, loop=False, clear=False)
-    print(triodecar.right_LDR())
     sleep(100)
+    print(triodecar.right_LDR())
     if triodecar.right_LDR() >= 450 and triodecar.right_LDR() <= 550 :
         display.show(Image.YES, delay=0, wait=True, loop=False, clear=False)
         sleep(1000)
@@ -33,6 +33,7 @@ while True:
 while True:
     D_value = int(math.fabs(triodecar.left_LDR() - triodecar.right_LDR()))
     display.show(D_value//10, delay=0, wait=True, loop=False, clear=False)
+    sleep(100)#microbit V1 添加此段延时会死机
     print(D_value)
     if D_value <= 10:
         display.show(Image.YES, delay=0, wait=True, loop=False, clear=False)
