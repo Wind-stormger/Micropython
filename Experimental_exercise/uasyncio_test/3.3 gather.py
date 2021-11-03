f4d0fc644dd8d11b11c00a7839c0ed8fd6fae01e
Wind-stormger <76462385+Wind-stormger@users.noreply.github.com>
2021-11-03 15:39:53 +0800

    Create 3.3 gather.py

---
+++ Experimental_exercise/uasyncio_test/3.3 gather.py
@@ -0,0 +1,46 @@
+try:
+    import uasyncio as asyncio
+except ImportError:
+    import asyncio
+
+async def barking(n):
+    print('Start barking')
+    for _ in range(6):
+        await asyncio.sleep(1)
+    print('Done barking.')
+    return 2 * n
+
+async def foo(n):
+    print('Start timeout coro foo()')
+    while True:
+        await asyncio.sleep(1)
+        n += 1
+    return n
+
+async def bar(n):
+    print('Start cancellable bar()')
+    while True:
+        await asyncio.sleep(1)
+        n += 1
+    return n
+
+async def do_cancel(task):
+    await asyncio.sleep(5)
+    print('About to cancel bar')
+    task.cancel()
+
+async def main():
+    tasks = [asyncio.create_task(bar(70))]
+    tasks.append(barking(21)) #Add to the end of the tasks array
+    tasks.append(asyncio.wait_for(foo(10), 7))
+    asyncio.create_task(do_cancel(tasks[0]))
+    res = None
+    try:
+        res = await asyncio.gather(*tasks, return_exceptions=True)
+    except asyncio.TimeoutError:  # These only happen if return_exceptions is False
+        print('Timeout')  # With the default times, cancellation occurs first
+    except asyncio.CancelledError:
+        print('Cancelled')
+    print('Result: ', res)
+
+asyncio.run(main())
