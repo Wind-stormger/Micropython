f81c959e2d5e6e309b1eda5422273242b93251d1
Wind-stormger <76462385+Wind-stormger@users.noreply.github.com>
2021-11-10 17:18:27 +0800

    Create OLED_display_IIC.py

---
+++ ESP32-SSD1306-OLED12864/OLED_display_IIC.py
@@ -0,0 +1,18 @@
+from machine import SoftI2C,Pin
+from ssd1306 import SSD1306_I2C
+
+class OLED_Show:
+    def __init__(self, sda_pin=16, scl_pin=17):        
+        self.i2c = SoftI2C(sda=Pin(sda_pin), scl=Pin(scl_pin))
+        self.oled = SSD1306_I2C(128, 64, self.i2c, addr=0x3c)
+        self.init_display()
+
+    def init_display(self,):
+        self.oled.text("Hello,I am wind.", 0,  32)    
+        self.oled.show()
+        
+def main():
+    oled_show = OLED_Show()
+
+if __name__=="__main__":
+    main()
