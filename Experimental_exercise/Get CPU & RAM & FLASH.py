0b89ba07cb2af0609c14a76c86eee0c1577f0686
Wind-stormger <76462385+Wind-stormger@users.noreply.github.com>
2021-11-15 15:23:19 +0800

    Update Get CPU & RAM & FLASH.py

--- Experimental_exercise/Get CPU & RAM & FLASH.py
+++ Experimental_exercise/Get CPU & RAM & FLASH.py
@@ -1,6 +1,7 @@
 import esp,machine
 # import gc
-print(machine.freq())#输出当前 CPU 频率
-print(gc.mem_alloc())#输出已用 RAM 字节数
-print(gc.mem_free())#输出可用 RAM 字节数
-print(esp.flash_size())#读取 FLASH 的总大小
+print("CPU frequency = {CPU}Mhz".format(CPU = machine.freq()/1000000))#输出当前 CPU 频率
+print("RAM used = {RAM_used}KB".format(RAM_used = gc.mem_alloc()/1024))#输出已用 RAM 
+print("RAM free = {RAM_free}KB".format(RAM_free = gc.mem_free()/1024))#输出可用 RAM 
+#print("RAM total = {RAM_total}KB".format(RAM_total = ( gc.mem_free() + gc.mem_alloc() ) / 1024))
+print("FLASH total = {FLASH_total}KB".format(FLASH_total = esp.flash_size()/1024))#输出 FLASH 的总容量
