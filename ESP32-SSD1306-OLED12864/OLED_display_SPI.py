2c6de25c622360239d0f68e0d310c3314f97c0a0
Wind-stormger <76462385+Wind-stormger@users.noreply.github.com>
2021-12-06 08:54:07 +0800

    Update OLED_display_SPI.py

--- ESP32-SSD1306-OLED12864/OLED_display_SPI.py
+++ ESP32-SSD1306-OLED12864/OLED_display_SPI.py
@@ -1,8 +1,13 @@
 from machine import SoftSPI,Pin
 from ssd1306 import SSD1306_SPI
 
-spi = SoftSPI(baudrate=10000000, polarity=1, phase=0, sck=Pin(11,Pin.OUT), mosi=Pin(12,Pin.OUT), miso=Pin(10))#sck=D0, mosi=D1
-oled = SSD1306_SPI(128, 64, spi, dc=Pin(14),res=Pin(13), cs=Pin(9))#dc=DC, res=RES, cs=CS&GND
+spi = SoftSPI(baudrate=10000000,
+              polarity=1,
+              phase=0,
+              sck=Pin(17,Pin.OUT),
+              mosi=Pin(16,Pin.OUT),
+              miso=Pin(10))#sck=D0, mosi=D1
+oled = SSD1306_SPI(128, 64, spi, dc=Pin(14),res=Pin(15), cs=Pin(13))#dc=DC, res=RES, cs=CS&GND
 
 def display():
     oled.text("Hello,I am wind.", 0,  32)    
